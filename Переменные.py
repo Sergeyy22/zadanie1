a342144c91d2926250fbf6b49af255478622e6d1
Sergeyy22 <167393140+Sergeyy22@users.noreply.github.com>
2024-06-11 23:14:49 +0700

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+done_dz = 12
+spent_time = 1.5
+name_course = "python"
+time_one_task = done_dz/spent_time
+print(f"Курс:{name_course}","Всего задач:{done_dz}",f"затрачено часов:{spent_time}",f"среднее время выполнения {time_one_task}")
