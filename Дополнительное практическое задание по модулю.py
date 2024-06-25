a1d582202061fc8d8fd1f905536f8d8f6d75db1f
Sergeyy22 <167393140+Sergeyy22@users.noreply.github.com>
2024-06-25 22:44:04 +0700

    Add files via upload

---
+++ Дополнительное практическое задание по модулю.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+
+def grades1():
+    for i in grades:
+        return i
+
+
+for j in students:
+    print({j:grades1()})
