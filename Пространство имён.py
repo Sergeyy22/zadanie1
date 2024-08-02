54f8be3b0c22ef00037ceb3301ed098610e40d12
Sergeyy22 <167393140+Sergeyy22@users.noreply.github.com>
2024-08-02 21:49:31 +0700

    Add files via upload

---
+++ Пространство имён.py
@@ -0,0 +1,19 @@
+calls = 0
+
+def count_calls():
+    calls = 12
+    print(calls)
+
+def string_info():
+    string = "garila"
+    print(string)
+
+def is_contains():
+    string = "человек"
+    list_to_search = ['оранжевый','маленький','вкусный']
+    return(string, list_to_search)
+
+string_info()
+print(is_contains())
+count_calls()
+print(calls)
