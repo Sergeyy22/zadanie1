755bdfabe457796b70cb06d254e280622c41ee31
Sergeyy22 <167393140+Sergeyy22@users.noreply.github.com>
2024-12-09 22:26:17 +0700

    Update Словари и множества.py

--- Словари и множества.py
+++ Словари и множества.py
@@ -1,10 +1,10 @@
-my_dict = {"Имя":"Сергей", 'Город':"Новосибирск","Возраст":"22"}
+my_dict = {"Имя": "Sergey", 'City': "Novosibirsk", "age": "22"}
 print(my_dict)
 print(my_dict["Имя"])
 print(my_dict.get("Рост"))
-my_dict.update({"Дата рождениия":27.09,"Год рождения": 2001})
+my_dict.update({"Birthday": 27.09, "year of birth": 2001})
 print(my_dict)
-del my_dict["Возраст"]
+del my_dict["age"]
 print(my_dict)
 my_set ={2, 3, 1, 2, 31, 21, 1, "MIR",2.9}
 print(my_set)
